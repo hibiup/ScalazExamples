adf44fe0a996c02b7ba912fc7c3b28767422456c
326487162 <Welcome@661>
2018-12-06 15:04:33 -0500

    Add two cases

--- src/test/scala/scalaz/examples/Scalaz_1_What_is_Monadic_test.scala
+++ src/test/scala/scalaz/examples/Scalaz_1_What_is_Monadic_test.scala
@@ -32,13 +32,23 @@ class Scalaz_1_What_is_Monadic_test extends FlatSpec{
         assert(concatABC == FullBag("Hello, World!"))
 
         /** Test EmptyBag*/
-        val emptyBag =
-            for {
+        val emptyBag = for {
                 a <- FullBag("Hello")
                 b <- EmptyBag: Bag[String]
                 c <- FullBag("World")
                 d <- FullBag("!")
             } yield ( a + b + c + d)
         assert(emptyBag == EmptyBag)
+
+        val emptyIntBag = for {
+                a <- FullBag(1)
+                b <- EmptyBag: Bag[Int]
+                c <- FullBag(3)
+            } yield ( a + b + c )
+        assert(emptyIntBag == EmptyBag)
+
+        FullBag(1).flatMap(a =>
+            (EmptyBag: Bag[Int]).flatMap(b =>
+                FullBag(3).map(c => a + b + c)))
     }
 }
